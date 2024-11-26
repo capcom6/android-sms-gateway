644e25183525e9eafee359bd45b594ea60afed7b
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-11-26 16:16:08 +0700

    [feature] allow to ignore battery optimizations

--- app/src/main/java/me/capcom/smsgateway/ui/SettingsFragment.kt
+++ app/src/main/java/me/capcom/smsgateway/ui/SettingsFragment.kt
@@ -1,7 +1,15 @@
 package me.capcom.smsgateway.ui
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import android.text.InputType
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.edit
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
@@ -28,6 +36,15 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
         findPreference<Preference>("transient.app_version")?.summary =
             "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            findPreference<Preference>("system")?.isEnabled = false
+            findPreference<Preference>("system.disable_battery_optimizations")?.summary =
+                getString(R.string.battery_optimizations_not_supported_on_this_device)
+        } else {
+            findPreference<Preference>("system.disable_battery_optimizations")?.summary =
+                if (isIgnoringBatteryOptimizations()) getString(R.string.disabled) else getString(R.string.enabled)
+        }
     }
 
     override fun onDisplayPreferenceDialog(preference: Preference) {
@@ -60,6 +77,45 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
 
         super.onDisplayPreferenceDialog(preference)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        if (preference.key == "system.disable_battery_optimizations") {
+            requestIgnoreBatteryOptimizations()
+            return true
+        }
+
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    private fun requestIgnoreBatteryOptimizations() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.battery_optimizations_not_supported_on_this_device),
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        if (isIgnoringBatteryOptimizations()) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.battery_optimizations_already_disabled),
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+        intent.data = Uri.parse("package:${requireContext().packageName}")
+        startActivity(intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isIgnoringBatteryOptimizations(): Boolean {
+        val powerManager = requireContext().getSystemService(Context.POWER_SERVICE) as PowerManager
+        return powerManager.isIgnoringBatteryOptimizations(requireContext().packageName)
     }
 
     companion object {
