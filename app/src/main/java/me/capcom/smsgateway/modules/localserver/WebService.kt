282f42b283c5b616261694f46832ebf1259c6a5a
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-12-26 15:32:32 +0700

    [localserver] move messages routes to sub-router

--- app/src/main/java/me/capcom/smsgateway/modules/localserver/WebService.kt
+++ app/src/main/java/me/capcom/smsgateway/modules/localserver/WebService.kt
@@ -9,7 +9,6 @@ import android.os.IBinder
 import android.os.PowerManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.toHttpDate
 import io.ktor.serialization.gson.gson
@@ -24,28 +23,22 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
-import io.ktor.server.request.receive
 import io.ktor.server.response.header
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
-import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.ktor.util.date.GMTDate
 import me.capcom.smsgateway.BuildConfig
 import me.capcom.smsgateway.R
-import me.capcom.smsgateway.domain.EntitySource
-import me.capcom.smsgateway.domain.ProcessingState
 import me.capcom.smsgateway.extensions.configure
 import me.capcom.smsgateway.modules.health.HealthService
 import me.capcom.smsgateway.modules.health.domain.Status
 import me.capcom.smsgateway.modules.localserver.domain.Device
-import me.capcom.smsgateway.modules.localserver.domain.PostMessageRequest
-import me.capcom.smsgateway.modules.localserver.domain.PostMessageResponse
 import me.capcom.smsgateway.modules.localserver.routes.LogsRoutes
+import me.capcom.smsgateway.modules.localserver.routes.MessagesRoutes
 import me.capcom.smsgateway.modules.localserver.routes.WebhooksRoutes
 import me.capcom.smsgateway.modules.messages.MessagesService
-import me.capcom.smsgateway.modules.messages.data.SendRequest
 import me.capcom.smsgateway.modules.notifications.NotificationsService
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
@@ -162,98 +155,12 @@ class WebService : Service() {
                             call.respond(listOf(device))
                         }
                     }
-                    route("/message") {
-                        post {
-                            val request = call.receive<PostMessageRequest>()
-                            if (request.message.isEmpty()) {
-                                call.respond(
-                                    HttpStatusCode.BadRequest,
-                                    mapOf("message" to "message is empty")
-                                )
-                            }
-                            if (request.phoneNumbers.isEmpty()) {
-                                call.respond(
-                                    HttpStatusCode.BadRequest,
-                                    mapOf("message" to "phoneNumbers is empty")
-                                )
-                            }
-                            if (request.simNumber != null && request.simNumber < 1) {
-                                call.respond(
-                                    HttpStatusCode.BadRequest,
-                                    mapOf("message" to "simNumber must be >= 1")
-                                )
-                            }
-                            val skipPhoneValidation =
-                                call.request.queryParameters["skipPhoneValidation"]
-                                    ?.toBooleanStrict() ?: false
-
-                            val sendRequest = SendRequest(
-                                EntitySource.Local,
-                                me.capcom.smsgateway.modules.messages.data.Message(
-                                    request.id ?: NanoIdUtils.randomNanoId(),
-                                    request.message,
-                                    request.phoneNumbers,
-                                    request.isEncrypted ?: false
-                                ),
-                                me.capcom.smsgateway.modules.messages.data.SendParams(
-                                    request.withDeliveryReport ?: true,
-                                    skipPhoneValidation = skipPhoneValidation,
-                                    simNumber = request.simNumber,
-                                    validUntil = request.validUntil,
-                                )
-                            )
-                            messagesService.enqueueMessage(sendRequest)
-
-                            val messageId = sendRequest.message.id
-
-                            call.respond(
-                                HttpStatusCode.Accepted,
-                                PostMessageResponse(
-                                    id = messageId,
-                                    state = ProcessingState.Pending,
-                                    recipients = request.phoneNumbers.map {
-                                        PostMessageResponse.Recipient(
-                                            it,
-                                            ProcessingState.Pending,
-                                            null
-                                        )
-                                    },
-                                    isEncrypted = request.isEncrypted ?: false,
-                                    mapOf(ProcessingState.Pending to Date())
-                                )
-                            )
+                    MessagesRoutes(get()).let {
+                        route("/message") {
+                            it.register(this)
                         }
-                        get("{id}") {
-                            val id = call.parameters["id"]
-                                ?: return@get call.respond(HttpStatusCode.BadRequest)
-
-                            val message = try {
-                                messagesService.getMessage(id)
-                                    ?: return@get call.respond(HttpStatusCode.NotFound)
-                            } catch (e: Throwable) {
-                                return@get call.respond(
-                                    HttpStatusCode.InternalServerError,
-                                    mapOf("message" to e.message)
-                                )
-                            }
-
-                            call.respond(
-                                PostMessageResponse(
-                                    message.message.id,
-                                    message.message.state,
-                                    message.recipients.map {
-                                        PostMessageResponse.Recipient(
-                                            it.phoneNumber,
-                                            it.state,
-                                            it.error
-                                        )
-                                    },
-                                    message.message.isEncrypted,
-                                    message.states.associate {
-                                        it.state to Date(it.updatedAt)
-                                    }
-                                )
-                            )
+                        route("/messages") {
+                            it.register(this)
                         }
                     }
                     WebhooksRoutes(get()).let {
