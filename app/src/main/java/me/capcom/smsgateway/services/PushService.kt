141c23e168f2a81752eaac8bf7d118101bab9cb9
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-01-02 15:03:22 +0700

    [push] add support for `MessagesExportRequested` event

--- app/src/main/java/me/capcom/smsgateway/services/PushService.kt
+++ app/src/main/java/me/capcom/smsgateway/services/PushService.kt
@@ -18,11 +18,14 @@ import me.capcom.smsgateway.modules.gateway.workers.RegistrationWorker
 import me.capcom.smsgateway.modules.gateway.workers.WebhooksUpdateWorker
 import me.capcom.smsgateway.modules.push.Event
 import me.capcom.smsgateway.modules.push.events.PushMessageEnqueuedEvent
+import me.capcom.smsgateway.modules.push.payloads.MessagesExportRequestedPayload
+import me.capcom.smsgateway.modules.receiver.ReceiverService
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
 import org.koin.core.component.inject
 
 class PushService : FirebaseMessagingService(), KoinComponent {
-    private val settingsHelper by lazy { SettingsHelper(this) }
+    private val settingsHelper by inject<SettingsHelper>()
 
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     private val eventBus by inject<EventBus>()
@@ -38,9 +41,22 @@ class PushService : FirebaseMessagingService(), KoinComponent {
             Log.d(this.javaClass.name, message.data.toString())
 
             val event = message.data["event"]?.let { Event.valueOf(it) } ?: Event.MessageEnqueued
+            val data = message.data["data"]
             when (event) {
                 Event.MessageEnqueued -> scope.launch { eventBus.emit(PushMessageEnqueuedEvent()) }
                 Event.WebhooksUpdated -> WebhooksUpdateWorker.start(this)
+                Event.MessagesExportRequested -> data
+                    ?.let {
+                        MessagesExportRequestedPayload.from(
+                            data
+                        )
+                    }
+                    ?.let { payload ->
+                        get<ReceiverService>().export(
+                            this,
+                            payload.since to payload.until
+                        )
+                    }
             }
         } catch (e: Throwable) {
             e.printStackTrace()
