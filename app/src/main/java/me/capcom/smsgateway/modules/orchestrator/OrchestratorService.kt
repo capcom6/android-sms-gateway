e900c67a492139e53a0f1dda4b70075f4b5668c4
Aleksandr Soloshenko <capcom@soft-c.ru>
2025-01-17 09:08:15 +0700

    [orchestrator] catch `ForegroundServiceStartNotAllowedException` while starting from background

--- app/src/main/java/me/capcom/smsgateway/modules/orchestrator/OrchestratorService.kt
+++ app/src/main/java/me/capcom/smsgateway/modules/orchestrator/OrchestratorService.kt
@@ -1,9 +1,12 @@
 package me.capcom.smsgateway.modules.orchestrator
 
+import android.app.ForegroundServiceStartNotAllowedException
 import android.content.Context
+import android.os.Build
 import me.capcom.smsgateway.modules.gateway.GatewayService
 import me.capcom.smsgateway.modules.localserver.LocalServerService
 import me.capcom.smsgateway.modules.logs.LogsService
+import me.capcom.smsgateway.modules.logs.db.LogEntry
 import me.capcom.smsgateway.modules.messages.MessagesService
 import me.capcom.smsgateway.modules.ping.PingService
 import me.capcom.smsgateway.modules.webhooks.WebHooksService
@@ -20,9 +23,25 @@ class OrchestratorService(
         logsSvc.start(context)
         messagesSvc.start(context)
         gatewaySvc.start(context)
-        localServerSvc.start(context)
         webHooksSvc.start(context)
-        pingSvc.start(context)
+
+        try {
+            localServerSvc.start(context)
+            pingSvc.start(context)
+        } catch (e: Throwable) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+                && e is ForegroundServiceStartNotAllowedException
+            ) {
+                logsSvc.insert(
+                    LogEntry.Priority.WARN,
+                    MODULE_NAME,
+                    "Can't start foreground services while the app is running in the background"
+                )
+                return
+            }
+
+            throw e;
+        }
     }
 
     fun stop(context: Context) {
