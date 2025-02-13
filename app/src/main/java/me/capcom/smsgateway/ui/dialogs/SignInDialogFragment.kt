e873d6694436e7d65eca38f32b2f0478eb08a606
Aleksandr Soloshenko <capcom@soft-c.ru>
2025-02-13 14:55:04 +0700

    [ui] prepare sign in flow

--- app/src/main/java/me/capcom/smsgateway/ui/dialogs/SignInDialogFragment.kt
+++ app/src/main/java/me/capcom/smsgateway/ui/dialogs/SignInDialogFragment.kt
@@ -2,32 +2,75 @@ package me.capcom.smsgateway.ui.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
 import me.capcom.smsgateway.R
 
 class SignInDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Use the Builder class for convenient dialog construction.
+        val view = requireActivity().layoutInflater.inflate(R.layout.fragment_sign_in, null)
         return AlertDialog.Builder(requireActivity())
             .apply {
-                setView(R.layout.fragment_sign_in)
+                setView(view)
                 setTitle("First start")
-                setMessage("To add the device to existing account please fill in the form and click Sign In. Or skip the form and click Register to create a new account.")
+                setMessage("To add the device to the existing account please fill in the form and click Sign In. Or skip the form and click Register to create a new account.")
                 setPositiveButton("Register") { dialog, id ->
-                    // START THE GAME!
+                    setFragmentResult(
+                        REQUEST_KEY,
+                        Bundle().apply {
+                            putString(KEY_RESULT_CODE, Result.Register.name)
+                        }
+                    )
                 }
                 setNeutralButton("Sign In") { dialog, id ->
-
+                    setFragmentResult(
+                        REQUEST_KEY,
+                        Bundle().apply {
+                            putString(KEY_RESULT_CODE, Result.SignIn.name)
+                            putString(
+                                KEY_USERNAME,
+                                view.findViewById<EditText>(R.id.editUsername).text.toString()
+                            )
+                            putString(
+                                KEY_PASSWORD,
+                                view.findViewById<EditText>(R.id.editPassword).text.toString()
+                            )
+                        }
+                    )
                 }
                 setNegativeButton("Cancel") { dialog, id ->
-                    // User cancelled the dialog.
+                    setFragmentResult(
+                        REQUEST_KEY,
+                        Bundle().apply {
+                            putString(KEY_RESULT_CODE, Result.Canceled.name)
+                        }
+                    )
                 }
             }
             .create()
     }
 
+    enum class Result {
+        Canceled,
+        SignIn,
+        Register,
+    }
+
     companion object {
+        const val REQUEST_KEY = "SignInDialogFragment"
+
+        private const val KEY_RESULT_CODE = "result_code"
+        private const val KEY_USERNAME = "username"
+        private const val KEY_PASSWORD = "password"
+
         fun newInstance(): SignInDialogFragment = SignInDialogFragment()
+
+        fun getResult(data: Bundle): Result =
+            Result.valueOf(data.getString(KEY_RESULT_CODE) ?: Result.Canceled.name)
+
+        fun getUsername(data: Bundle): String = requireNotNull(data.getString(KEY_USERNAME))
+        fun getPassword(data: Bundle): String = requireNotNull(data.getString(KEY_PASSWORD))
     }
 }
