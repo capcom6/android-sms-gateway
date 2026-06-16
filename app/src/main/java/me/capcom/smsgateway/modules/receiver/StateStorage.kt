a4354d365f95decf269be2adc4d5624813682318
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-06-16 13:39:46 +0700

    [receiver] add sms content observer fallback

--- app/src/main/java/me/capcom/smsgateway/modules/receiver/StateStorage.kt
+++ app/src/main/java/me/capcom/smsgateway/modules/receiver/StateStorage.kt
@@ -10,9 +10,14 @@ class StateStorage(
         get() = storage.get<Long>(MMS_LAST_PROCESSED_ID) ?: 0
         set(value) = storage.set(MMS_LAST_PROCESSED_ID, value)
 
+    var smsLastProcessedID: Long
+        get() = storage.get<Long>(SMS_LAST_PROCESSED_ID) ?: 0
+        set(value) = storage.set(SMS_LAST_PROCESSED_ID, value)
+
     companion object {
         private val PREFIX = "state."
 
         private val MMS_LAST_PROCESSED_ID = PREFIX + "last_processed_id"
+        private val SMS_LAST_PROCESSED_ID = PREFIX + "sms_last_processed_id"
     }
 }
